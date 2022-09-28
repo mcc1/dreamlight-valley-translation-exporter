d891858cbd6d17122afb022339da2379f6bc6735
mcc <mcc@mccma.cc>
2022-09-28 16:15:15 +0800

    first release

---
+++ pack.py
@@ -0,0 +1,75 @@
+import os
+import re
+from pathlib import Path
+
+# from . import decode_length
+
+exp = re.compile(r"\"(.+?)\", \"(.+?)\"$", re.MULTILINE | re.UNICODE | re.DOTALL)
+
+
+def encode_length(l: int) -> bytes:
+    result = bytearray()
+
+    while True:
+        if (l // 128) == 0:
+            result.append(l % 128)
+            break
+        result.append(l % 128 + 128)
+        l = l // 128
+    return result
+
+
+def encode_data(key: str, val: str) -> bytes:
+    # 0A total_len 0A key_len key 12 val_len val
+    result = bytearray(b"\x0a")
+    key_len = len(key.encode("utf8"))
+    enc_key_len = encode_length(key_len)
+    val_len = len(val.encode("utf8"))
+    enc_val_len = encode_length(val_len)
+    total_len = key_len + val_len
+    if len(key) > 0:
+        total_len += len(enc_key_len) + 1
+    if len(val) > 0:
+        total_len += len(enc_val_len) + 1
+    result.extend(encode_length(total_len))
+    if len(key) > 0:
+        result.extend(b"\x0a" + enc_key_len + key.encode("utf8"))
+    if len(val) > 0:
+        result.extend(b"\x12" + enc_val_len + val.encode("utf8"))
+    return result
+
+
+def encode_file(input_file: Path, output_location=None):
+    with open(input_file, encoding="utf8") as fp:
+        if output_location is not None:
+            if not output_location.parent.exists():
+                os.makedirs(output_location.parent)
+            wp = open(output_location.with_suffix(".locbin"), "wb")
+        tmpline = ""
+        lines = fp.readlines()
+        for line in lines:
+            if (data := exp.match(line)) is not None:
+                key = data.group(1)
+                val = data.group(2)
+                wp.write(encode_data(key, val))
+            else:
+                tmpline += line
+                if (data := exp.match(tmpline)) is not None:
+                    tmpline = ""
+                    key = data.group(1)
+                    val = data.group(2)
+                    wp.write(encode_data(key, val))
+        wp.close()
+
+
+if __name__ == "__main__":
+    for path in Path("zh_TW").glob("**/*.txt"):
+        encode_file(path, "working" / path.relative_to("./zh_TW"))
+
+    import zipfile
+
+    with zipfile.ZipFile(
+        "LocDB_zh-CN.zip", mode="w", compression=zipfile.ZIP_DEFLATED, compresslevel=9
+    ) as zf:
+        for path in Path("working").glob("**/*.locbin"):
+            zf.write(path)
---
+++ unpack.py
@@ -0,0 +1,76 @@
+import os
+from pathlib import Path
+
+
+def decode_length(n: bytes) -> int:
+    result = 0
+    if len(n) < 1:
+        raise ValueError("Invalid length")
+    for i in range(1, len(n) + 1):
+        result += (n[i - 1] - 1) * pow(128, i - 1)
+    result += 1
+    return result
+
+
+def decode_data(kv: bytes) -> (str, str):
+    key = val = b""
+    key_length = val_length = 0
+    key_bit = val_bit = 1
+
+    key_pos = kv.find(b"\x0a")
+    # check have key or not
+    if key_pos != -1:
+        # try to decode key_length
+        while (l := decode_length(kv[key_pos + 1 : key_pos + 1 + key_bit])) < 5000:
+            key_length = l
+            key_bit += 1
+        key_bit -= 1
+        key = kv[key_pos + key_bit + 1 : key_pos + key_bit + key_length + 1]
+
+    val_pos = kv.find(b"\x12", key_pos + key_bit + key_length)
+    # check have val or not
+    if val_pos != -1:
+        # try to decode val_length
+        while (l := decode_length(kv[val_pos + 1 : val_pos + 1 + val_bit])) < len(kv):
+            val_length = l
+            val_bit += 1
+        val_bit -= 1
+        val = kv[val_pos + val_bit + 1 : val_pos + val_bit + val_length + 1]
+    # if key_length + key_bit + val_length + val_bit + 2 != len(kv):
+    #     print(key_length, key_bit, val_length, val_bit, len(kv))
+    #     raise ValueError("key value length mismatch")
+    try:
+        return key.decode("utf8"), val.decode("utf8")
+    except Exception as e:
+        print(key, val, e)
+        return "Error", "Error"
+
+
+def decode_file(input_file: Path, output_location=None):
+    buffer_size = 10
+    with open(input_file, "rb") as fp:
+        if output_location is not None:
+            if not output_location.parent.exists():
+                os.makedirs(output_location.parent)
+            wp = open(output_location.with_suffix(".txt"), "w+", encoding="utf8")
+        while (header := fp.read(buffer_size)) != b"":
+            total_meta_pos = header.index(b"\x0a")
+            if (key_meta_pos := header.find(b"\x0a", total_meta_pos + 1)) == -1:
+                # missing key
+                key_meta_pos = header.find(b"\x12", total_meta_pos + 1)
+            total_length = decode_length(header[total_meta_pos + 1 : key_meta_pos])
+            # reset fp to key_meta_pos
+            fp.seek(-(buffer_size - key_meta_pos), 1)
+            key, val = decode_data(fp.read(total_length))
+            if output_location == None:
+                print(key, val)
+            else:
+                wp.write(f'"{key}", "{val}"\n')
+        wp.close()
+
+
+if __name__ == "__main__":
+    for path in Path("./src").glob("**/*.locbin"):
+        if path.name == "tutorial.locbin":
+            continue
+        decode_file(path, "zh_TW" / path.relative_to("./src"))
